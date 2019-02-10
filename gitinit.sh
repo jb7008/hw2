504060d75428de6ac7fd3baebd3a0db161d0cd75
Josh Booth <jb7008@ship.edu>
2019-02-09 21:02:55 -0500

    first commit

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git config user.email "jb7008@ship.edu"
+git config user.name "Josh Booth"
+git commit -m "first commit"
+git remote add origin https://github.com/jb7008/hw2.git
+git push -u origin master
